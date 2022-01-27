4dfc546c8d3ba9794b909fff1c8ec7c6058aff9f
LBBassani <lorenabassani12@gmail.com>
2022-01-26 21:45:14 -0300

    Usa as liberdades de movimento

--- src/Domain/MovingEntity.hpp
+++ src/Domain/MovingEntity.hpp
@@ -23,7 +23,7 @@ struct MovingEntity : public Entity{
     float velocity = 0.0f;
     MoveConfigurations x_moveConfigurations;
     MoveConfigurations y_moveConfigurations;
-    MoveConfigurations angular_moveConfigurations;    
+    MoveConfigurations angular_moveConfigurations;
 
     MovingEntity(MoveConfigurations x_config = MoveConfigurations(), MoveConfigurations y_config = MoveConfigurations(), MoveConfigurations ang_config = MoveConfigurations()){
         this->is_movable = true;
@@ -48,15 +48,32 @@ struct MovingEntity : public Entity{
         float   new_x_position = this->transform.position.x + x_moveConfigurations.velocity * deltaTime,
                 new_y_position = this->transform.position.y + y_moveConfigurations.velocity * deltaTime;
         
-        // Atualiza a posição em x se estiver nos limites
-        if (new_x_position > x_moveConfigurations.min 
-            && new_x_position < x_moveConfigurations.max) 
+        this->last_position.x = this->transform.position.x;
+        // Atualiza a posição em x se estiver nos limites e estiver se movendo em uma direção permitida
+        if (    new_x_position > x_moveConfigurations.min                                   // Posição é maior que a mínima permitida
+                && new_x_position < x_moveConfigurations.max                                // Posiçao é menor que a máxima permitida
+                && (
+                    (x_moveConfigurations.velocity > 0.0f && moveLiberty.direita)           // Estar se movendo pra direta (velocity positiva) e ter liberdade pra isso 
+                    || (x_moveConfigurations.velocity < 0.0f && moveLiberty.esquerda)       // Estar se movendo pra esquerda (velocity negativo) e ter liberdade pra isso
+                )
+            ){ 
                 this->transform.position.x = new_x_position;
-
-        // Atualiza a posição em y se estiver nos limites
-        if (new_y_position > y_moveConfigurations.min 
-            && new_y_position < y_moveConfigurations.max) 
+            }
+        
+        this->last_position.y = this->transform.position.y;
+        // Atualiza a posição em y se estiver nos limites e estiver se movendo em uma direção permitida
+        if (    new_y_position > y_moveConfigurations.min                                   // Posição é maior que a mínima permitida
+                && new_y_position < y_moveConfigurations.max                                // Posiçao é menor que a máxima permitida
+                && (
+                    (y_moveConfigurations.velocity > 0.0f && moveLiberty.para_cima)         // Estar se movendo pra cima (velocity positiva) e ter liberdade pra isso 
+                    || (y_moveConfigurations.velocity < 0.0f && moveLiberty.para_baixo)     // Estar se movendo pra baixo (velocity negativo) e ter liberdade pra isso
+                )
+            ){
+                #if defined TEST
+                //std::cout << this->last_position.y << std::endl;
+                #endif
                 this->transform.position.y = new_y_position;
+            }
         
     }
 
