33d24f39b2f9a504c1e26aa4de5a4e5850938bcc
LBBassani <lorenabassani12@gmail.com>
2022-01-27 10:29:29 -0300

    Model tem hitbox offset

--- src/Domain/Model.hpp
+++ src/Domain/Model.hpp
@@ -13,6 +13,7 @@ struct Model{
     std::string nome;
     Shape *shape = nullptr;
     Shape *hitbox = nullptr;
+    glm::vec2 hitbox_offset;
     glm::vec3 color;
 
     virtual std::string getNome() { return nome; };
@@ -22,14 +23,15 @@ struct Model{
     virtual Shape *getShape() { return shape; };
     virtual void setShape(Shape *shape){ this->shape = shape; };
     virtual Shape *getHitbox() { return hitbox; };
-    virtual void setHitbox(Shape *hitbox) { this->hitbox = hitbox; };
+    virtual void setHitbox(Shape *hitbox, glm::vec2 hitbox_offset = {0.0f, 0.0f}) { this->hitbox = hitbox; this->hitbox_offset = hitbox_offset; };
+    virtual glm::vec2 getHitboxOffset(){ return hitbox_offset; };
 
     virtual void draw(){
         glColor3f(color.x, color.y, color.z);
         if (shape) shape->draw();
         #if defined TEST
-        glColor3f(1.0f, 1.0f, 1.0f);
-        if (hitbox) hitbox->draw();
+        //glColor3f(1.0f, 1.0f, 1.0f);
+        //if (hitbox) hitbox->draw();
         #endif
     }
 };
