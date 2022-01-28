ee0acf0abfbfab260f25adb7da5be9834ba856bc
LBBassani <lorenabassani12@gmail.com>
2022-01-28 14:25:45 -0300

    Não imprime mais hitbox

--- src/Domain/Model.hpp
+++ src/Domain/Model.hpp
@@ -30,9 +30,9 @@ struct Model{
         glColor3f(color.x, color.y, color.z);
         if (shape) shape->draw();
         #if defined TEST
-            glColor3f(1.0f, 1.0f, 1.0f);
+            /* glColor3f(1.0f, 1.0f, 1.0f);
             glTranslatef(hitbox_offset.x, hitbox_offset.y, 0.0f);
-            if (hitbox) hitbox->draw();
+            if (hitbox) hitbox->draw(); */
         #endif
     }
 };
