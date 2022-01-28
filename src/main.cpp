60cd37d92245f98faa40df158363641f5c402401
LBBassani <lorenabassani12@gmail.com>
2022-01-28 12:42:07 -0300

    Primeiro calculo das posições absolutas O primeiro calculo das matrizes de transformação

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@ int main(int argc, char *argv[]){
     SceneTree sceneTree;
     sceneTree.root = ParserSVG::parseSVG(arquivo);
 
+    sceneTree.root->updateSelfAndChildren();
     sceneTree.updateHitboxMapping();
 
     #if defined TEST
