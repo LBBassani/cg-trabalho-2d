2945d239db158b48fe3c8377f261fea3b0322554
LBBassani <lorenabassani12@gmail.com>
2022-01-28 12:40:25 -0300

    Testes de ponteiros null e mensagens no terminal

--- src/Domain/HitboxMapping.hpp
+++ src/Domain/HitboxMapping.hpp
@@ -23,15 +23,19 @@ struct HitboxMapping{
     }
 
     virtual void update_hitbox(){
-        if(!entity_ptr) return;
+        if(!entity_ptr || !entity_ptr->hitbox) return;
 
         Rect* hitbox = dynamic_cast<Rect*>(entity_ptr->hitbox);
         
-        glm::vec4 canto_dir = entity_ptr->transform.getLocalModelMatrix()*glm::vec4((hitbox->width)/2 + entity_ptr->hitbox_offset.x, (hitbox->height) + entity_ptr->hitbox_offset.y, 0.0f, 1.0f);
-        glm::vec4 canto_esq = entity_ptr->transform.getLocalModelMatrix()*glm::vec4(-(hitbox->width)/2 + entity_ptr->hitbox_offset.x, 0 + entity_ptr->hitbox_offset.y, 0.0f, 1.0f);
+        glm::vec4 canto_dir = entity_ptr->transform.modelMatrix*glm::vec4((hitbox->width)/2 + entity_ptr->hitbox_offset.x, (hitbox->height) + entity_ptr->hitbox_offset.y, 1.0f, 1.0f);
+        glm::vec4 canto_esq = entity_ptr->transform.modelMatrix*glm::vec4(-(hitbox->width)/2 + entity_ptr->hitbox_offset.x, 0 + entity_ptr->hitbox_offset.y, 1.0f, 1.0f);
 
         this->canto_sup_direito = glm::vec2(canto_dir.x, canto_dir.y);
         this->canto_inf_esquerdo = glm::vec2(canto_esq.x, canto_esq.y);
+
+        #if defined TEST
+            //std::cout << "Rect w : " << hitbox->width << " h: " << hitbox->height << " " << this->entity_ptr->getNome() << " p1: (" << this->canto_inf_esquerdo.x << ", " << this->canto_inf_esquerdo.y << ") p2: (" << this->canto_sup_direito.x << ", " << this->canto_sup_direito.y << ") "<< std::endl;
+        #endif
 
     }
 
@@ -43,10 +47,15 @@ struct HitboxMapping{
             std::max(this->canto_sup_direito.y, another_hitbox.canto_sup_direito.y)
             - std::min(this->canto_inf_esquerdo.y, another_hitbox.canto_inf_esquerdo.y)
         ),
-                self_area = (this->canto_inf_esquerdo.x - this->canto_sup_direito.x)
-                *(this->canto_sup_direito.y - this->canto_inf_esquerdo.y);
+                self_area = abs(this->canto_inf_esquerdo.x - this->canto_sup_direito.x)
+                *abs(this->canto_sup_direito.y - this->canto_inf_esquerdo.y);
+            
+        #if defined TEST
+            /* Rect* hitbox = dynamic_cast<Rect*>(entity_ptr->hitbox);
+            std::cout << "Rect w : " << hitbox->width << " h: " << hitbox->height << " " << this->entity_ptr->getNome() << " p1: (" << this->canto_inf_esquerdo.x << ", " << this->canto_inf_esquerdo.y << ") p2: (" << this->canto_sup_direito.x << ", " << this->canto_sup_direito.y << ") Area: " << self_area << std::endl; */
+        #endif
 
-        return std::min(abs(overlapping_area/self_area),100.0f);
+        return abs(overlapping_area/self_area) == INFINITY ? 0 : abs(overlapping_area/self_area) ;
 
     }
 
