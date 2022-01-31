227ddaf5ee4d307c4ee8d1b421169757afbd75dd
LBBassani <lorenabassani12@gmail.com>
2022-01-31 11:29:16 -0300

    Arquivo passado pela linha de comando

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <stdexcept>
 
 #include "Configuration/OpenGLConfig.hpp"
 #include "Configuration/OpenGLStarter.hpp"
@@ -15,7 +16,17 @@
 
 int main(int argc, char *argv[]){
 
-    std::string arquivo("../scenario/arena_teste.svg");
+    std::string arquivo;
+
+    #if defined TEST
+        arquivo = "../scenario/arena_teste.svg";
+    #else
+        if(argc >= 2) arquivo = argv[1];
+        else throw std::runtime_error(std::string("Not enought arguments: expected .svg file to create level"));
+    #endif
+
+    std::cout << arquivo << std::endl;
+
     OpenGLConfig openGLConfig = OpenGLConfig("Trabalho 2D",700, 700, 500, 500);
     OpenGLStarter openGLStarter = OpenGLStarter(openGLConfig, arquivo);
     openGLStarter.setInstance(&openGLStarter);
